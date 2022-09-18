c90e9d8422651424d8624e302061bb6e8bc6d54e
Krystian Florkowski <krystianf06@gmail.com>
2022-09-18 14:20:15 +0200

    login1

---
+++ Questify_backend/service/schema/regist.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+    {
+        password: {
+          type: String,
+          required: [true, 'Password is required'],
+        },
+        email: {
+          type: String,
+          required: [true, 'Email is required'],
+          unique: true,
+        },
+        token: {
+          type: String,
+          default: null,
+        },
+        verify: {
+          type: Boolean,
+          default: false,
+        },
+        verificationToken: {
+          type: String,
+          required: [true, "Verify token is required"],
+      },
+      },
+      { versionKey: false, timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema, "users");
+
+module.exports = User;
